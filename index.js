d6498de2a5c8368caf29a5302795edf86f9ae599
iqbalsalik <sisalik84@gmail.com>
2023-03-20 15:15:02 +0530

    sync await promis

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-const posts = []
 
-let updateLastUserActivityTime = new Promise((res) => {
-    setTimeout(() => {
-        let updateTime = new Date();
-        res(updateTime)
-    }, 1000);
-})
 
-let count = 1
-let createPost = new Promise((res) => {
-    setTimeout(() => {
-        posts.push({ title: `POST ${count}` })
-        count++
-        res()
-    }, 2000);
-})
+const creatingPost = async function () {
 
-function printPost() {
-    console.log(posts)
+    const posts = [{title: "posts"}]
 
+    const createPost = new Promise((res,rej) => {
+        setTimeout(() => {
+            posts.push({ title: `POST1` })
+            res(posts)
+        }, 2000);
+    })
+
+    let updateLastUserActivityTime = new Promise((res) => {
+        setTimeout(() => {
+            let updateTime = new Date();
+            res(`last updated time: ${updateTime}`)
+        }, 5000);
+    })
+
+    const deletePost =  new Promise((res, rej) => {
+            if (posts.length!=0) {
+                posts.pop()
+                res()
+            } else{
+                rej("ERROR")
+            }
+        })
+
+    function printPost() {
+        return posts
+
+    }
+
+ let post1 = await createPost;
+    console.log(post1);
+    let time = await updateLastUserActivityTime;
+    console.log(time);
+    // let deletepo = await deletePost;
+    let print = printPost();
+    console.log(print)
+
+    return post1
 }
 
-function deletePost(){
-    return new Promise((res,rej)=>{
-        if(posts.length){
-            posts.pop()
-            res()
-        }else{
-            rej("ERROR")
-        }
-    })
-}
-
-Promise.all([createPost, updateLastUserActivityTime]).then((msg) => {
-    printPost()
-    console.log(msg)
-    deletePost().then(()=>{
-        printPost()
-    })
-})
+creatingPost()
